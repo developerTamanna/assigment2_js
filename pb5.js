3dec056656448e1e54285f7bb22bdbd9fe11f495
developerTamanna <astamanna57@gmail.com>
2025-02-19 14:05:51 +0600

    pblm5

---
+++ pb5.js
@@ -0,0 +1,25 @@
+
+function  calculateSleepTime( times ) {
+    if(!Array.isArray(times)){
+        return 'Invalid'
+    }
+// console.log(times)
+let sum = 0
+for(let value of times){
+    if(typeof value!=='number'){
+        return 'Invalid'
+    }
+    sum = sum + value;
+    // console.log(sum)
+}
+
+const hours = Math.floor(sum / 3600);
+const minutes = Math.floor((sum % 3600)/60);
+const second = sum % 60;
+return {hours, minutes, second};
+}
+
+const time = calculateSleepTime([5600])
+
+
+console.log(time);
